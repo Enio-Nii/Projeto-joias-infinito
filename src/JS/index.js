a7fa66ef635859a4645a485310ceb1f2e9c721e8
Enio-Nii <eniocullen21@gmail.com>
2024-08-11 10:40:28 -0300

    Add files via upload

---
+++ src/JS/index.js
@@ -0,0 +1,25 @@
+const botoesCarrosel = (document.querySelectorAll(".botao"));
+const imagens = document.querySelectorAll(".imagem");
+const informacoes = document.querySelectorAll(".informacoes");
+
+botoesCarrosel.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+     
+     const botaoSelecionado = document.querySelector(".selecionado");
+ 
+     botaoSelecionado.classList.remove("selecionado");
+     botao.classList.add("selecionado");
+ 
+     const imagemAtiva = document.querySelector(".ativa");
+     imagemAtiva.classList.remove("ativa");
+ 
+     imagens[indice].classList.add("ativa");
+     
+     const informacoesAtiva = document.querySelector(".informacoes.ativa");
+     informacoesAtiva.classList.remove("ativa");
+     
+     informacoes[indice].classList.add("ativa");
+ 
+     });
+ 
+ });
